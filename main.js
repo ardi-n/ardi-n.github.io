988a8762f06d9363c565d2bb94f8e5fd35f50564
Adrian Nowicki <me@adrian-nowicki.com>
2015-10-20 17:30:26 +0200

    change

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 (function() {
     console.log("EJSTEM");
+    var gauth = gapi.auth2.init({
+        client_id: "1093122655002-s8qee1hvia7jg14di3radf8jcj3q2g9c.apps.googleusercontent.com"
+    });
     gapi.signin2.render("g-signin", {
         app_package_name: "com.div.minicarracing",
         onsuccess: function(arg) {
