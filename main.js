50f928bd26014422f9e4f4de3c0230769d67004b
Adrian Nowicki <me@adrian-nowicki.com>
2015-10-20 18:02:35 +0200

    change

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 (function() {console.log("gapi", gapi);
-    var gauth = gapi.auth2.init({
-        client_id: "129154954257-du73uhf205pfm0vqu0mmjnpuv0gpp9gi.apps.googleusercontent.com"
+    gapi.load("auth2", function() {
+        var gauth = gapi.auth2.init({
+            client_id: "129154954257-du73uhf205pfm0vqu0mmjnpuv0gpp9gi.apps.googleusercontent.com"
+        });
     });
-    gapi.signin2.render("g-signin", {
+    
+    gapi.signin2.render("g-signin2", {
         app_package_name: "com.div.minicarracing",
         onsuccess: function(arg) {
             console.log("success", arg);
